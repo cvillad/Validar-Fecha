4b0e24b6fb31e19b75eb7e0356a20d8d6365766a
cvillad <cvillad@uninorte.edu.co>
2019-11-02 16:22:12 -0500

    second commit

--- src/date/Date.java
+++ src/date/Date.java
@@ -19,34 +19,20 @@ public class Date {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner sc=new Scanner(System.in);
-        while(true){
+        int option;
+        do{
+            Scanner sc=new Scanner(System.in);
             System.out.print("Ingrese fecha a validar: ");
-            String date = sc.nextLine();
-            Pattern pattern=Pattern.compile("\\d\\d/\\d\\d/\\d\\d\\d\\d");
+            String date = sc.nextLine();///0\\d|1[0-2]/\\d\\d\\d\\d
+            Pattern pattern=pattern= Pattern.compile("([0-2]\\d|3[01])/(0\\d|1[0-2])/\\d\\d\\d\\d");
             Matcher matcher=pattern.matcher(date);
             System.out.print(date);
-            if (matcher.find() && checkDate(date)) {
+            if (matcher.matches()) {
                 System.out.println(" es válido");
             }else System.out.println(" no es válido");
-        }
-    }
-    
-    public static boolean checkDate(String date){
-        int day=Integer.parseInt(date.substring(0,2));
-        int month=Integer.parseInt(date.substring(3,5));
-        int year=Integer.parseInt(date.substring(8));
-        boolean sw=false;
-        if (year>1) {
-            if (day>=1 && day<=31 && month>=1 && month<=12 && month!=2) {
-                sw=true;
-            }else if(month==2 && day<=29){
-                if(year%4!=0 && day<29){
-                    sw=true;
-                }else if(year%4==0)sw=true;
-            }
-        }
-        return sw;
+            System.out.print("1. Continuar    2. Salir");
+            option=sc.nextInt();
+        }while(option==1);
     }
     
 }
